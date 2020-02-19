809583b2a88ebc19f4d4fbdc0d3b3f2ae4aa7979
Ryo Taga <taga@n-di.co.jp>
2020-02-19 13:36:30 +0900

    不要な行を削除した。

--- src/components/History.vue
+++ src/components/History.vue
@@ -2,7 +2,6 @@
   <div :class="['history', className ? className : '']">
     <p :class="['history-close', className ? 'is-hidden' : '']" @click="hideHistory">
       <font-awesome-icon icon="chevron-down" />
-      <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg> -->
     </p>
     <ul class="history-list">
       <li
